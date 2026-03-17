98dba9d1e70ca3de115856f3c2875b0d540ae35c
Nathan Sudmeier <nathansudmeier@gmail.com>
2026-03-17 13:20:15 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -196,7 +196,7 @@ export default function Layout({ children, currentPageName }) {
       {/* Main content */}
       <main className="pt-16 lg:pl-56 pb-20 lg:pb-0 relative min-h-screen">
         <div 
-          className="fixed inset-0 lg:left-56 top-16 bottom-0 z-0 pointer-events-none"
+          className="lg:hidden fixed inset-0 top-16 bottom-0 z-0 pointer-events-none"
           style={{
             backgroundImage: "url('https://media.base44.com/images/public/69ad40ab17517be2ed782cdd/98a8a794b_Appbackground.jpg')",
             backgroundSize: "cover",
