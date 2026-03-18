a60bc00e28890e6acfda7ee5c9c8b4efc28242e9
Nathan Sudmeier <nathansudmeier@gmail.com>
2026-03-18 14:37:07 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -210,7 +210,8 @@ export default function Layout({ children, currentPageName }) {
           {isSpeelsterUser ? (
             <>
               <NavLink item={mainNavItems[0]} currentPageName={currentPageName} />
-              <NavLink item={mainNavItems[3]} currentPageName={currentPageName} />
+              <NavLink item={mainNavItems[1]} currentPageName={currentPageName} />
+              <NavLink item={mainNavItems[4]} currentPageName={currentPageName} />
               <NavLink item={{ name: "Spelprincipes", icon: Grid3x3, page: "Spelprincipes" }} currentPageName={currentPageName} />
               <DeveloperGroup currentPageName={currentPageName} />
             </>
