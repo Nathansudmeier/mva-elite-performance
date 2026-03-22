70bbd328707572a143313f43b3dc3b5ca633b184
Nathan Sudmeier <nathansudmeier@gmail.com>
2026-03-22 13:13:05 +0000

    File changes

--- src/components/spelprincipes/RichTextEditor.jsx
+++ src/components/spelprincipes/RichTextEditor.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from "react";
-...
+
 export default function RichTextEditor({ value, onChange, placeholder }) {
   const editorRef = useRef(null);
   const isInitialized = useRef(false);
